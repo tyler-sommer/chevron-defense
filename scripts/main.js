9b9de7edc2a386f430dd6941ac18d0ba54135e30
Tyler Sommer <sommertm@gmail.com>
2014-07-07 01:41:08 -0700

    Wired up start button

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,19 @@
+require.config({
+  urlArgs: "bust=" + (new Date()).getTime()
+});
 require(['game/board', 'game/wave'],
   function(Game, Wave) {
     var width  = Math.floor(window.innerWidth / 25);
     var height = Math.floor(window.innerHeight / 25);
     var board = new Game.GameBoard(document.getElementById('container'), width, height);
 
-    var delayBetweenWaves = 2;
-    var maxWaveHeight     = 5;
-    var generator = new Wave.WaveGenerator(board, delayBetweenWaves, maxWaveHeight);
-
+    var startButton = document.getElementById('start-button');
+    startButton.addEventListener('click', function() {
+      var maxDelayBetweenWaves = 3;
+      var maxWaveHeight     = 10;
+      var generator = new Wave.WaveGenerator(board, maxDelayBetweenWaves, maxWaveHeight);
+      startButton.parentNode.removeChild(startButton);
+    });
+    
     board.run();
   });
